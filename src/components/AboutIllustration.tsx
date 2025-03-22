e8d144d7b548aac5dba1bb018430a96a312e46e6
Bernardo Knoblauch <bekno.adm@gmail.com>
2025-03-22 21:29:02 +0100

    Fix SVG loading by moving to src/assets

--- src/components/AboutIllustration.tsx
+++ src/components/AboutIllustration.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import Image from 'next/image';
+import aboutIllustration from '@/assets/about-illustration.svg';
 
 export default function AboutIllustration() {
   return (
     <div className="relative w-full h-full">
       <img
-        src="/about-illustration.svg"
+        src={aboutIllustration}
         alt="BEKNO About Illustration"
         className="w-full h-auto"
       />
