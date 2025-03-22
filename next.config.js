4cc27eab7e2467681669664ac01177aa8d822bc5
Bernardo Knoblauch <bekno.adm@gmail.com>
2025-03-22 21:20:33 +0100

    Fix SVG handling in production build

--- next.config.js
+++ next.config.js
@@ -3,9 +3,17 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
+    domains: ['localhost'],
   },
   basePath: process.env.NODE_ENV === 'production' ? '/bekno-landing-page' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/bekno-landing-page/' : '',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
